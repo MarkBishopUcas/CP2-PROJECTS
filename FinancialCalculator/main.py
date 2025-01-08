ab0567f61e949ca572f64517016569dbdf94a1af
MarkBishopUcas <mark.bishop@ucas-edu.net>
2025-01-08 10:49:56 -0700

    Added a few variables, forgot to add input

---
+++ FinancialCalculator/main.py
@@ -0,0 +1,6 @@
+current_money=print("how much money do you have?": )
+def savings_goal ():
+    savings_goal_amount=print("How much money would you like to save up for?": )
+    print(" do you get paid weekly, bi-weekly, or monthly?": )
+    income=print("How much money do you make per week or month?": )
+    print("it will take you")
