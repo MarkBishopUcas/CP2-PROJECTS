1713757589da9c21f7d372b6d6c1bc8565c56e9a
MarkBishopUcas <mark.bishop@ucas-edu.net>
2025-01-10 07:11:25 +0000

    Added full functionallity (not bug tested yet)

--- FinancialCalculator/main.py
+++ FinancialCalculator/main.py
@@ -1,11 +1,13 @@
 # Mark Bishop Financial Calculator
 
-def savings_goal (current_money):
-    if int(input("This is the savings goal calculator, you set an item, how much money it costs, how often you will deposit money, and how much, and we will tell you how long it will take to save  for it. Continue(1) Go back to menu(2)")) == 1():
+def savings_goal_func():
+    savings_weekly_monthly=0
+    if int(input("This is the savings goal calculator, you set an item, how much money it costs, \nhow often you will deposit money, and how much, and we will tell you how long \nit will take to save for it. Continue(1) Go back to menu(2)")) == 1:
 
         savings_for=input("What item are you saving for? ")
-        savings_goal_amount=int(input("How much money would you like to save up for? "))
-        savings_weekly_monthly=int(input("How often will you deposit money? weekly(1), bi-weekly(2), or monthly(3) "))
+        savings_goal_amount=float(input("How much money would you like to save up for? $"))
+        while savings_weekly_monthly not in [1, 2, 3]:
+            savings_weekly_monthly=int(input("How often will you deposit money? weekly(1), bi-weekly(2), or monthly(3) "))
         if savings_weekly_monthly == (1):
             savings_weekly_monthly=('week')
         elif savings_weekly_monthly == (2):
@@ -13,17 +15,98 @@ def savings_goal (current_money):
             savings_goal_amount=(savings_goal_amount*2)
         else:
             savings_weekly_monthly=('month')
-        savings_income=int(input("How much money do you plan to deposit each time? "))
+        savings_income=float(input("How much money do you plan to deposit each time? $"))
         print('calculating...')
-        savings_goal_amount=(savings_goal_amount-current_money)
         savings_time=(savings_goal_amount/savings_income)
         print(f"it will take {savings_time} {savings_weekly_monthly}s to save up for your: {savings_for}")
     else:
         print("going back to main menu...")
 
+def compound_interest_func():
+    if int(input("This is the coumpound interest calculator, you enter in your principle amount, \nwhich is the amount you depositit, or the amount you loaned, then you enter your \nanual interest rate, next you enter how many times it compounds per year, finnaly \nyou enter the amount of years you're saving it for, then it will output the interest on that money \n1. Continue \n2. Go back to menu\nEnter the number corresponding to your choice: ")) == 1:
+        principle_amount=float(input("What is the principle amount? $"))
+        anual_intrest_rate=float(input("what is the anual intrest rate? enter as a decimal: "))
+        times_compounded=int(input("How many times is it compounded per year? "))
+        time_period=int(input("How many years will it be saved for? "))
+        final_amount=principle_amount*(1+anual_intrest_rate/times_compounded)**(times_compounded*time_period)
+        print(f"After {time_period} years, the final amount will be {final_amount}")
+    else:
+        print("going back to main menu...")
+
+def budget_allocator_func():
+    if int(input("Welcome to the Budget Allocator!\nEnter your total budget, divide it into sections, and assign percentages to each.\n1. Continue\n2. Go back\nEnter your choice: ")) == 1:
+        budget=float(input("How much money do you want to budget? $"))
+        number_of_items=int(input("how many sections do you want to split your budget into? "))
+        budget_list=[]
+        for i in range(number_of_items):
+            item=input(f"add item {i+1} to the list: ")
+            budget_list.append(item)
+        percentages=[]
+        for i in range(number_of_items):
+            percentage=float(input(f"assign a % to {budget_list[i]}: "))
+            percentages.append(percentage)
+        while sum(percentages) > 100:
+            print(f"Warning: Total percentage is {sum(percentages)}%. The total must equal to, or less than 100%.")
+            percentages=[]
+            for i in range(number_of_items):
+                percentage=float(input(f"assign a % to {budget_list[i]}: "))
+                percentages.append(percentage)
+        print("Here is your budget allocation")
+        for i in range(number_of_items):
+            amount=(percentages[i]/100)*budget
+            print(f"{budget_list[i]}: ${amount:.2f} ({percentages[i]}%)")
+    else:
+        print("going back to main menu...")
+def discount_calculator_func():
+    if int(input("Welcome to the Discount Calculator!\nEnter the original price, the discount percentage, and find out how much you save.\n1. Continue\n2. Go back\nEnter your choice: ")) == 1:
+        original_price=float(input("Enter the original price "))
+        discount_percent=float(input("Enter the dsicount % "))
+        discount_amount=(original_price*discount_percent)/100
+        final_price=(original_price-discount_amount)
+        print(f"You saved ${discount_amount:.2f}, and the final price is ${final_price:.2f}.")
+    else:
+        print("going back to main menu...")
+
+def tip_calculator_func():
+    if int(input("Welcome to the Tip Calculator!\nEnter the bill amount and the tip percentage, and it will calculate the tip and total.\n1. Continue\n2. Go back\nEnter your choice: ")) == 1:
+        price_amount=float(input("What was the cost of the bill? "))
+        tip_percent=float(input("What % would you like to tip: "))
+        tip_amount=(price_amount*tip_percent)/100
+        print(f"For a {tip_percent}% tip, you need to leave ${tip_amount:.2f}")
+    else:
+        print("going back to main menu...")
+    
 def main():
-    print("Welcome to the financial calculator. Before you begin, when presented with a multiple choice question, please awnser with the number to the left of the option you want to select.")
-    current_money=int(input("What is your current ballance? "))
-    services=int(input("which of our services would you like to usse? Savings goal calculator(1), Compound interest calculator(2), Budget allocator(3), Discount calculator(4), Tip calculator(5) "))
-    if services==(1):
-        savings_goal(current_money)
+    services=0 
+    print("Welcome to the financial calculator.")
+    while True:
+        while services not in [1, 2, 3, 4, 5]:
+            print("please choose an option")
+            print("1. Savings goal calculator")
+            print("2. Compound interest calculator")
+            print("3. Budget allocator")
+            print("4. Discount calculator")
+            print("5. Tip calculator")
+            services=int(input("Enter the number corresponding to your choice: "))
+            if services not in [1, 2, 3, 4, 5]:
+                print("Please choose a number 1-5")
+        
+        else:
+            if services==(1):
+                savings_goal_func()
+            elif services==(2):
+                compound_interest_func()
+            elif services==(3):
+                budget_allocator_func()
+            elif services==(4):
+                discount_calculator_func()
+            elif services==(5):
+                tip_calculator_func()
+        if int(input("Would you like to use any of our other services?\n1. Yes\n2. No\nEnter the number corresponding to your choice: "))==2:
+            print("Thank you for using the financial calculator!")
+            break
+        else:
+            services=0 
+
+if __name__=="__main__":
+    main()
