2c247ab641d45a721655e94d15fe7a02dd62443a
MarkBishopUcas <mark.bishop@ucas-edu.net>
2025-01-10 06:24:22 +0000

    Added compound interest func, and budget allocator

--- FinancialCalculator/main.py
+++ FinancialCalculator/main.py
@@ -1,23 +1,29 @@
 # Mark Bishop Financial Calculator
 
-def savings_goal ():
-    savings_for=input("What item are you saving for? ")
-    savings_goal_amount=int(input("How much money would you like to save up for? "))
-    savings_weekly_monthly=int(input("How often will you deposit money? weekly(1), bi-weekly(2), or monthly(3) "))
-    if savings_weekly_monthly == (1):
-        savings_weekly_monthly=('week')
-    elif savings_weekly_monthly == (2):
-        savings_weekly_monthly=('week')
-        savings_goal_amount=(savings_goal_amount*2)
+def savings_goal (current_money):
+    if int(input("This is the savings goal calculator, you set an item, how much money it costs, how often you will deposit money, and how much, and we will tell you how long it will take to save  for it. Continue(1) Go back to menu(2)")) == 1():
+
+        savings_for=input("What item are you saving for? ")
+        savings_goal_amount=int(input("How much money would you like to save up for? "))
+        savings_weekly_monthly=int(input("How often will you deposit money? weekly(1), bi-weekly(2), or monthly(3) "))
+        if savings_weekly_monthly == (1):
+            savings_weekly_monthly=('week')
+        elif savings_weekly_monthly == (2):
+            savings_weekly_monthly=('week')
+            savings_goal_amount=(savings_goal_amount*2)
+        else:
+            savings_weekly_monthly=('month')
+        savings_income=int(input("How much money do you plan to deposit each time? "))
+        print('calculating...')
+        savings_goal_amount=(savings_goal_amount-current_money)
+        savings_time=(savings_goal_amount/savings_income)
+        print(f"it will take {savings_time} {savings_weekly_monthly}s to save up for your: {savings_for}")
     else:
-        savings_weekly_monthly=('month')
-    savings_income=int(input("How much money do you plan to deposit each time? "))
-    print('calculating...')
-    savings_goal_amount=(savings_goal_amount-current_money)
-    savings_time=(savings_goal_amount/savings_income)
-    print(f"it will take {savings_time} {savings_weekly_monthly}s to save up for your: {savings_for}")
+        print("going back to main menu...")
 
 def main():
     print("Welcome to the financial calculator. Before you begin, when presented with a multiple choice question, please awnser with the number to the left of the option you want to select.")
     current_money=int(input("What is your current ballance? "))
     services=int(input("which of our services would you like to usse? Savings goal calculator(1), Compound interest calculator(2), Budget allocator(3), Discount calculator(4), Tip calculator(5) "))
+    if services==(1):
+        savings_goal(current_money)
