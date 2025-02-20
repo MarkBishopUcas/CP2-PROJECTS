703f2a21ad2ac9b468b2c2aca3f0a4e3675ba800
MarkBishopUcas <mark.bishop@ucas-edu.net>
2025-02-20 10:51:04 -0700

    txt reading setup

--- to_do_list/main.py
+++ to_do_list/main.py
@@ -1,1 +1,6 @@
+
 #initial commit
+#Create a to do list (Kept on a txt file). Add items to the to do list. Mark item as complete. Delete item from to do list
+with open("to_do_list/to_do_list.txt", "r") as file:
+    txt_reader = (file.read())
+    print(txt_reader)
