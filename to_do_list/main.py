3c2208c4d90787dc0e4c93faaec7f70482daeb0c
MarkBishopUcas <mark.bishop@ucas-edu.net>
2025-02-21 10:50:39 -0700

    added main func and wip other funcs

--- to_do_list/main.py
+++ to_do_list/main.py
@@ -1,6 +1,23 @@
 
 #initial commit
 #Create a to do list (Kept on a txt file). Add items to the to do list. Mark item as complete. Delete item from to do list
+dict_todo = {}
 with open("to_do_list/to_do_list.txt", "r") as file:
     txt_reader = (file.read())
-    print(txt_reader)
+    lst_todo = txt_reader.split(",")
+
+    for i in range(2,len(lst_todo),2):
+        dict_todo.update({lst_todo[i]: lst_todo[i+1]})
+    print(dict_todo)
+
+def add_item():
+    print("wip")
+
+def modify_item():
+    print("wip")
+
+def complete():
+    print("wip")
+
+def main():
+    print("wip")
