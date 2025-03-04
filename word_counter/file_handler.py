fc691530cb234eb68a704ccd3579e80ad1b3967f
MarkBishopUcas <mark.bishop@ucas-edu.net>
2025-03-04 05:15:54 +0000

    commit so i can download it so i can run it on my pc

--- word_counter/file_handler.py
+++ word_counter/file_handler.py
@@ -1,1 +1,8 @@
-#innitial commit
+def file_select():
+    file_path = input("Please copy and paste the path of your file: ") # Variable storing the file path
+    
+    def write_to_file(file_path):
+        with open(file_path, "w+") as file:
+            file.write("This is a test.")  # Example content written to the file
+    
+    write_to_file(file_path)  # Call the function with the variable as the parameter
--- word_counter/main.py
+++ word_counter/main.py
@@ -1,1 +1,13 @@
-#innitial commit
+import file_handler
+
+def main():
+    try:
+        selection = int(input("\nWhere would you like to go?\n(1) File select\n(2) Word counter\n(3) Time display\n(4) view/edit file\nType the number corrosponding to your selection: "))
+    except:
+        print("\nPlease only enter in a whole number")
+        main()
+    if selection == 1:
+        file_handler.file_select()
+    elif selection == 2:
+        print("WIP")
+main()
